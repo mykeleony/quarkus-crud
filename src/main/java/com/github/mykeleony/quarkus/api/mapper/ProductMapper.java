5250e8d66cffc7a8d48f5aa8e5cd8e2e8f5afd96
myke amorim <myke.amorim@usp.br>
2023-06-09 22:23:35 -0300

    Annotated mapper with @ApplicationScoped
    Similar to Spring @Component annotation.

--- src/main/java/com/github/mykeleony/quarkus/api/mapper/ProductMapper.java
+++ src/main/java/com/github/mykeleony/quarkus/api/mapper/ProductMapper.java
@@ -2,9 +2,11 @@ package com.github.mykeleony.quarkus.api.mapper;
 
 import com.github.mykeleony.quarkus.api.model.ProductInput;
 import com.github.mykeleony.quarkus.domain.model.Product;
+import jakarta.enterprise.context.ApplicationScoped;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 
+@ApplicationScoped
 @AllArgsConstructor
 public class ProductMapper {
 
